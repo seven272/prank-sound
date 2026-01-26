c1f0a08843a0b10ec89afe34dafaf16720a2f906
seven272 <i727i@mail.ru>
2026-01-26 18:09:20 +0300

    new

--- client/src/config/var.js
+++ client/src/config/var.js
@@ -1,6 +1,6 @@
-const DOCKER_PORT = 5002
+const DOCKER_PORT = 5000
 const LOCAL_PORT = 5000
-const DOCKER_FILE_URL = 'http://localhost:5002'
+const DOCKER_FILE_URL = 'http://localhost:5000'
 const LOCAL_FILE_URL = 'http://localhost:5000'
 
 export {DOCKER_PORT, LOCAL_PORT,  DOCKER_FILE_URL,  LOCAL_FILE_URL}
