6190626d942e5db68333e10f6992f3387f12b756
seven272 <i727i@mail.ru>
2026-02-05 13:05:18 +0300

    fix vk api

--- client/src/utils/vkStorage.js
+++ client/src/utils/vkStorage.js
@@ -54,7 +54,7 @@ const getAllItemsStorage = () => {
 // сохранение состояние просмотренного онбординга
 const setOnboardingShown = () => {
   return bridge.send('VKWebAppStorageSet', {
-    key: 'onboardingShown',
+    key: 'onboardingShownPrankSound',
     value: '1',
   })
 }
@@ -62,7 +62,7 @@ const setOnboardingShown = () => {
 // получение состояние просмотренного онбординга
 const getOnboardingShown = async () => {
   const result = await bridge.send('VKWebAppStorageGet', {
-    keys: ['onboardingShown'],
+    keys: ['onboardingShownPrankSound'],
   })
   // получением значение первого элемента массива обьектов keys
   const resultValue = result.keys[0].value
