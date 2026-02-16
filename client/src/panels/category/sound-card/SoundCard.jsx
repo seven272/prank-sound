ca99f7e46bc7ed4d03a2cf7398beb34dd57e4b4e
seven272 <i727i@mail.ru>
2026-02-16 13:46:47 +0300

    sound card

--- client/src/panels/category/sound-card/SoundCard.jsx
+++ client/src/panels/category/sound-card/SoundCard.jsx
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux'
+import { MdOutlineArrowBackIos } from 'react-icons/md'
+import { MdOutlineArrowForwardIos } from 'react-icons/md'
 
 import styles from './SoundCard.module.css'
 import PrankImg from '../../../assets/images/prank.png'
@@ -6,18 +8,22 @@ import PrankImg from '../../../assets/images/prank.png'
 const SoundCard = () => {
   const { currentSound } = useSelector((state) => state.sound)
   const URL = import.meta.env.VITE_PUBLIC_URL
-  
+
   return (
     <section className={styles.section}>
       <span className={styles.title}>
         {currentSound.title || 'какой-то звук'}
       </span>
-      <div className={styles.img_wrap}>
-        <img
-          src={`${URL}/${currentSound?.imageUrl}` || PrankImg}
-          alt="изображение карточки звука"
-          className={styles.img}
-        />
+      <div className={styles.content_wpap}>
+        <MdOutlineArrowBackIos className={styles.arrow}/>
+        <div className={styles.img_wrap}>
+          <img
+            src={`${URL}/${currentSound?.imageUrl}` || PrankImg}
+            alt="изображение карточки звука"
+            className={styles.img}
+          />
+        </div>
+        <MdOutlineArrowForwardIos className={styles.arrow}/>
       </div>
     </section>
   )
