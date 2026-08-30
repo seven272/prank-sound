c73b4000d9930f3808fdcc51cafac5957f57a8fe
seven272 <i727i@mail.ru>
2026-08-30 16:16:45 +0300

    update

--- server/src/config/corsOptions.js
+++ server/src/config/corsOptions.js
@@ -9,31 +9,6 @@
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'http://prank-sound.ru',
-      'https://prank-sound.ru',
-      'https://185.251.89.77',
-      'http://185.251.89.77',
-      'https://vk.com',
-      'https://vk.ru',
-      /vk\.com$/,
-      /vk\.me$/,
-
-    ]
-
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('CORS policy violation'))
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  optionsSuccessStatus: 200, //предоставляет статус-код для успешного разрешения запросов
-  credentials: true, // Разрешает передачу cookies
-}
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
       'http://localhost:3000',
       'http://prank-sound.ru',
       'https://prank-sound.ru',
